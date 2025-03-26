548784a9e54068495a2ca7ebb5fac10de4053860
M0hamed1221 <mohamedkhaledk1221@gmail.com>
2025-03-26 22:27:58 +0200

    Add project files.

---
+++ WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,80 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+namespace MVC.Controllers
+{
+    public class MoviesController:Controller
+    {
+        // Action
+
+        //public string GetMoive(int? id, string name)
+        //{
+        //    if (id is not null)
+        //    {
+        //        return $"Movie ---------------{id}/{name}";
+        //    }
+
+        //    return "No Moive";
+        //}
+        //public ContentResult GetMoive(int? id, string name)
+        //{
+        //    //ContentResult contentResult = new ContentResult();
+        //    //contentResult.StatusCode = 9999;
+        //    //contentResult.ContentType = "text/html";
+        //    //contentResult.Content =$"Movie ---------------{id}/{name}";
+
+        //    //return contentResult;
+        //    return Content($"Movie ---------------{id}/{name}", "text/html");
+
+        //}
+        public IActionResult GetMoive(int? id, string name)
+        {
+            //ContentResult contentResult = new ContentResult();
+            //contentResult.StatusCode = 9999;
+            //contentResult.ContentType = "text/html";
+            //contentResult.Content =$"Movie ---------------{id}/{name}";
+
+            //return contentResult;
+            if(id==0)
+            {
+                return BadRequest();
+
+            }
+            else if (id < 10)
+            {
+                return NotFound();
+
+            }
+            else
+                return Content($"Movie ---------------{id}/{name}", "text/html");
+
+        }
+        public IActionResult Redirect()
+        {
+            return Redirect("");
+        }
+        public IActionResult RedirectToGetMovie()
+        {
+            return RedirectToAction("index", "MovieController",new {id=30,name="x-man"});
+        }
+        public IActionResult RedirectToRoute()
+        {
+            return RedirectToRoute("Default");
+        }
+        public IActionResult TestmodelBinding(int? id, string name)
+        {
+            return Content($"Movie ---------------{id}/{name}", "text/html");
+
+        }
+        public string index (int? id )   
+        {
+
+            if (id is not null)
+            {
+                return $"index ---------------{id}";
+            }
+
+            return "No index";
+        }
+    } 
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            //app.MapGet("/", () => "Hello World!");
+
+            app.MapGet("/MKM", async context =>
+            {
+                await context.Response.WriteAsync("mkm !"); /*Satic segment*/
+            });
+
+            app.MapGet(pattern:"/Fixed{name}", requestDelegate:async context =>
+            {
+                var name = context.Request.RouteValues["name"];
+                await context.Response.WriteAsync($"you are variable segment and your name is {name} !"); /*Varible segment*/
+            });
+
+            app.MapControllerRoute(name: "Mohamed",
+                pattern: "{ Controller=Movies}/{Action=GetMoives}/{id}" );
+
+            app.MapControllerRoute(name:"default",
+                pattern:"{ Controller}/{Action}/{id?}"
+                
+                );
+            app.MapControllerRoute(name: "default",
+               pattern: "{ Controller}/{Action}/{id:int?}",
+               defaults:new {Controller="Movies", action= "index" },
+               constraints:new {id=new IntRouteConstraint()}
+               );
+            app.MapControllerRoute(name: "default",
+              pattern: "{ Controller}/{Action}/{id:regex(\\d{{2}}$)?}/{name:alpha}",
+              defaults: new { Controller = "Movies", action = "index" },
+              constraints: new { id = new IntRouteConstraint() }
+              );
+            app.MapControllerRoute(name: "default",
+               pattern: "{ Controller}/{Action}/{id:int?}"
+               //defaults: new { Controller = "Movies", action = "index" },
+               //constraints: new { id = new IntRouteConstraint() }
+               );
+
+            app.Run();
+        }
+    }
+}
