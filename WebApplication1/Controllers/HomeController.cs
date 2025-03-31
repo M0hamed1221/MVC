38f37e3eaea92f30461d9400d91da874ec40ffe9
M0hamed1221 <mohamedkhaledk1221@gmail.com>
2025-03-31 18:42:54 +0200

    views

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*BaseURL/Home/index(Action)*/
+        public IActionResult Index()
+        {
+            return View();// Retrun view with the same name as the action name
+            //return View(new Movie());// Retrun view with the same name as the action name and BIND the Model
+            //return View("MKM");// Retrun view with the Provided name as the action name
+            //return View("MKM", new Movie());
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+            
+        }
+        public IActionResult About()
+        {
+            return View();
+
+        }
+        public IActionResult Contact()
+        {
+            return View();
+
+        }
+    }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,42 +16,50 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
             app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+            });
             //app.MapGet("/", () => "Hello World!");
 
-            app.MapGet("/MKM", async context =>
-            {
-                await context.Response.WriteAsync("mkm !"); /*Satic segment*/
-            });
+            //app.MapGet("/MKM", async context =>
+            //{
+            //    await context.Response.WriteAsync("mkm !"); /*Satic segment*/
+            //});
 
-            app.MapGet(pattern:"/Fixed{name}", requestDelegate:async context =>
-            {
-                var name = context.Request.RouteValues["name"];
-                await context.Response.WriteAsync($"you are variable segment and your name is {name} !"); /*Varible segment*/
-            });
+            //app.MapGet(pattern:"/Fixed{name}", requestDelegate:async context =>
+            //{
+            //    var name = context.Request.RouteValues["name"];
+            //    await context.Response.WriteAsync($"you are variable segment and your name is {name} !"); /*Varible segment*/
+            //});
 
-            app.MapControllerRoute(name: "Mohamed",
-                pattern: "{ Controller=Movies}/{Action=GetMoives}/{id}" );
+            //app.MapControllerRoute(name: "Mohamed",
+            //    pattern: "{ Controller=Movies}/{Action=GetMoives}/{id}" );
 
-            app.MapControllerRoute(name:"default",
-                pattern:"{ Controller}/{Action}/{id?}"
-                
-                );
-            app.MapControllerRoute(name: "default",
-               pattern: "{ Controller}/{Action}/{id:int?}",
-               defaults:new {Controller="Movies", action= "index" },
-               constraints:new {id=new IntRouteConstraint()}
-               );
-            app.MapControllerRoute(name: "default",
-              pattern: "{ Controller}/{Action}/{id:regex(\\d{{2}}$)?}/{name:alpha}",
-              defaults: new { Controller = "Movies", action = "index" },
-              constraints: new { id = new IntRouteConstraint() }
-              );
-            app.MapControllerRoute(name: "default",
-               pattern: "{ Controller}/{Action}/{id:int?}"
-               //defaults: new { Controller = "Movies", action = "index" },
-               //constraints: new { id = new IntRouteConstraint() }
-               );
+            //app.MapControllerRoute(name:"default",
+            //    pattern:"{ Controller}/{Action}/{id?}"
+
+            //    );
+            //app.MapControllerRoute(name: "default",
+            //   pattern: "{ Controller}/{Action}/{id:int?}",
+            //   defaults:new {Controller="Movies", action= "index" },
+            //   constraints:new {id=new IntRouteConstraint()}
+            //   );
+            //app.MapControllerRoute(name: "default",
+            //  pattern: "{ Controller}/{Action}/{id:regex(\\d{{2}}$)?}/{name:alpha}",
+            //  defaults: new { Controller = "Movies", action = "index" },
+            //  constraints: new { id = new IntRouteConstraint() }
+            //  );
+            //app.MapControllerRoute(name: "default",
+            //   pattern: "{ Controller}/{Action}/{id:int?}"
+            //   //defaults: new { Controller = "Movies", action = "index" },
+            //   //constraints: new { id = new IntRouteConstraint() }
+            //   );
 
             app.Run();
         }
